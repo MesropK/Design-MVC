3f052550f77af011b3ea5f61d8afbc177dd16f65
Mesrop-Kareyan <mesrop.kareyan@bazillionbeings.com>
2017-04-04 18:32:36 +0400

    First commit

---
+++ Design-MVC/Design-MVC/AllImagesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  Design-MVC
+//
+//  Created by Mesrop Kareyan on 4/4/17.
+//  Copyright © 2017 Mesrop Kareyan. All rights reserved.
+//
+
+import UIKit
+
+class AllImagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Design-MVC/Design-MVC/MyImagesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  Design-MVC
+//
+//  Created by Mesrop Kareyan on 4/4/17.
+//  Copyright © 2017 Mesrop Kareyan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
